ede7d447a88af6d7f129ffb8dc6365103392371e
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-02-10 11:21:01 -0800

    herpa durrr

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -14,9 +14,20 @@ export default class Ball{
   reset(){
       this.x = this.boardWidth /2;
       this.y = this.boardHeight /2;
+
+      this.vy = 0;
+
+      while( this.vy === 0){
+          this.vy = Math.floor(Math.random() * 10 - 5);
+      }
+
+      this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
   render(svg){
+      this.x += this.vx;
+      this.y += this.vy;
+
 
       let ball = document.createElementNS( SVG_NS, 'circle')
       ball.setAttributeNS(null, 'r', this.radius)
